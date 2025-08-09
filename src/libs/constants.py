64786ea08100602f3550bdebc7cab6d594cac267
Rohan Vashisht <81112205+RohanVashisht1234@users.noreply.github.com>
2025-08-09 15:05:53 +0530

    Update constants.py

--- src/libs/constants.py
+++ src/libs/constants.py
@@ -14,6 +14,8 @@ PACKAGES_URLS = (
     "https://api.github.com/search/repositories?q=topic:zig-package+fork:true+created:2024-08-09..2025-02-08&page=2&per_page=100",
     "https://api.github.com/search/repositories?q=topic:zig-package+fork:true+created:2025-02-09..2025-08-08&page=1&per_page=100",
     "https://api.github.com/search/repositories?q=topic:zig-package+fork:true+created:2025-02-09..2025-08-08&page=2&per_page=100",
+    "https://api.github.com/search/repositories?q=topic:zig-package+fork:true+created:2025-02-09..2025-08-08&page=3&per_page=100",
+    "https://api.github.com/search/repositories?q=topic:zig-package+fork:true+created:2025-08-09..2025-12-31&page=1&per_page=100",
 )
 
 PROGRAMS_URLS = (
@@ -54,6 +56,8 @@ PROGRAMS_URLS = (
     "https://api.github.com/search/repositories?q=topic:zig+fork:true+created:2025-02-09..2025-08-08&page=2&per_page=100",
     "https://api.github.com/search/repositories?q=topic:zig+fork:true+created:2025-02-09..2025-08-08&page=3&per_page=100",
     "https://api.github.com/search/repositories?q=topic:zig+fork:true+created:2025-02-09..2025-08-08&page=4&per_page=100",
+    "https://api.github.com/search/repositories?q=topic:zig+fork:true+created:2025-02-09..2025-08-08&page=5&per_page=100",
+    "https://api.github.com/search/repositories?q=topic:zig+fork:true+created:2025-08-09..2025-12-31&page=1&per_page=100",
 )
 
 GITHUB_API_KEY = os.getenv("GH_API_KEY")
