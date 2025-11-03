a6efb7ba2a6d09e77a4b919b47b9f2c392eafff9
Rohan Vashisht <81112205+RohanVashisht1234@users.noreply.github.com>
2025-11-04 00:53:30 +0530

    Add additional URLs for zig-package API queries

--- src/libs/constants.py
+++ src/libs/constants.py
@@ -16,6 +16,7 @@ PACKAGES_URLS = (
     "https://api.github.com/search/repositories?q=topic:zig-package+fork:true+created:2025-02-09..2025-08-08&page=2&per_page=100",
     "https://api.github.com/search/repositories?q=topic:zig-package+fork:true+created:2025-02-09..2025-08-08&page=3&per_page=100",
     "https://api.github.com/search/repositories?q=topic:zig-package+fork:true+created:2025-08-09..2025-12-31&page=1&per_page=100",
+    "https://api.github.com/search/repositories?q=topic:zig-package+fork:true+created:2025-08-09..2025-12-31&page=2&per_page=100",
 )
 
 PROGRAMS_URLS = (
@@ -59,6 +60,7 @@ PROGRAMS_URLS = (
     "https://api.github.com/search/repositories?q=topic:zig+fork:true+created:2025-02-09..2025-08-08&page=5&per_page=100",
     "https://api.github.com/search/repositories?q=topic:zig+fork:true+created:2025-08-09..2025-12-31&page=1&per_page=100",
     "https://api.github.com/search/repositories?q=topic:zig+fork:true+created:2025-08-09..2025-12-31&page=2&per_page=100",
+    "https://api.github.com/search/repositories?q=topic:zig+fork:true+created:2025-08-09..2025-12-31&page=3&per_page=100",
 )
 
 GITHUB_API_KEY = os.getenv("GH_API_KEY")
